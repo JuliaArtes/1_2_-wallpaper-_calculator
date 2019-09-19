d9a26440f03b038b003ca4ca058a4f3601811da8
MaratArkhipov <maratkzn2010@yandex.ru>
2019-09-19 23:47:03 +0400

    расчет количества обоев без стыковки

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        float roomLengthM = 15f ; //требуется ввести длину комнаты в метрах
-        float roomWidthM = 6f; //требуется ввести ширину комнаты в метрах
-        float roomHeight = 2 ;//требуется ввести высоту комнаты в метрах
-        float rollWidthM = 7f;//требуется ввести ширину рулона обоев
-        float rollLenghM= 5f; //требуется ввести длину рулона в метрах
-        float roomPerimeterM = roomLengthM*roomWidthM;
-        float numberOfPieces =  rollLenghM%roomHeight;
-        System.out.println(numberOfPieces);
-
+        int roomLengthSm = 500; // enter the length of the room incentimeters
+        int roomWidthSm = 600; // enter the width of the room in centimeters
+        int roomHeightSm = 220;//  enter the height of the room in centimeters
+        int rollWidthSm = 70;//enter the wallpaper width  in centimeters
+        int rollLenghSm = 1000; //enterthe roll length in centimeters
+        int roomPerimeterSm = 2 * (roomLengthSm + roomWidthSm);
+        int numberOfPiecesInRoll = rollLenghSm / roomHeightSm;
+        int numberOfPiecesOnRoom = roomPerimeterSm / rollWidthSm;
+        int numberOfRoll= (numberOfPiecesOnRoom+1)/numberOfPiecesInRoll+1;
+        System.out.println("Периметр  "+roomPerimeterSm);
+        System.out.println("Число кусков на комнату  "+numberOfPiecesOnRoom);
+        System.out.println("Число кусков в рулоне  "+numberOfPiecesInRoll);
+        System.out.println("Необходимое количество рулонов  "+numberOfRoll);
     }
 }
